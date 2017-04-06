959ebfd32a3c9ccc74f7305222725f3bba22c90a
tohemu <tohemu@hotmail.com>
2017-04-05 21:29:11 -0500

    Se añade nuevo Calendario

---
+++ app/src/main/java/net/tohemu/educa/Calendario2Activity.java
@@ -0,0 +1,81 @@
+package net.tohemu.educa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Calendario2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_calendario2);
+    }
+
+    public void MostrarHome(View v) {
+        Intent i = new Intent(getBaseContext(), HomeActivity.class);
+        startActivity(i);
+    }
+
+    public void MostrarInforme(View v) {
+        Intent i = new Intent(getBaseContext(), InformesActivity.class);
+        startActivity(i);
+    }
+
+    public void MostrarTareas(View v) {
+        Intent i = new Intent(getBaseContext(), Tareas3Activity.class);
+        startActivity(i);
+    }
+
+    public void MostrarMensajeria(View v) {
+        Intent i = new Intent(getBaseContext(), MensajeriaActivity.class);
+        startActivity(i);
+    }
+
+    public void MostrarAcerca(View v){
+        Intent i = new Intent(getBaseContext(), AboutActivity.class);
+        startActivity(i);
+    }
+
+    public void MostrarMenuConfig(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.config_menu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent i;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_alumno:
+                        i = new Intent(getBaseContext(), CambioUserActivity.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.nav_passwd:
+                        i = new Intent(getBaseContext(), CambioPasswdActivity.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.nav_config:
+                        i = new Intent(getBaseContext(), ConfigActivity.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.nav_about:
+                        i = new Intent(getBaseContext(), AboutActivity.class);
+                        startActivity(i);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popup.show();
+    }
+}
