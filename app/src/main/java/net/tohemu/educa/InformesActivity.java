66ce7d3bf242ee5a8bcce2e7b1d45818cc134f97
tohemu <tohemu@hotmail.com>
2017-05-02 23:12:02 -0500

    sincronización del proyecto

--- app/src/main/java/net/tohemu/educa/InformesActivity.java
+++ app/src/main/java/net/tohemu/educa/InformesActivity.java
@@ -1,18 +1,35 @@
 package net.tohemu.educa;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import net.tohemu.educa.models.EventoDTO;
+import net.tohemu.educa.models.RespuestaDTO;
+import net.tohemu.educa.utils.Constantes;
+import net.tohemu.educa.utils.RequestHttpGet;
+import net.tohemu.educa.utils.UtilsLib;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class InformesActivity extends AppCompatActivity {
 
@@ -140,8 +157,9 @@ public class InformesActivity extends AppCompatActivity {
     }
 
     public void MostrarCalendario(View v) {
-        Intent i = new Intent(getBaseContext(), Calendario2Activity.class);
-        startActivity(i);
+        //Intent i = new Intent(getBaseContext(), Calendario2Activity.class);
+        //startActivity(i);
+        getEventosCalendario();
     }
 
     public void MostrarMensajeria(View v) {
@@ -187,5 +205,56 @@ public class InformesActivity extends AppCompatActivity {
         });
 
         popup.show();
+    }
+
+    private void getEventosCalendario(){
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setIndeterminate(true);
+        pd.setMessage("Cargando...");
+
+        new RequestHttpGet(getApplicationContext(), new net.tohemu.educa.utils.CallBack() {
+            @Override
+            public void onResult(JSONObject response) {
+                pd.dismiss();
+                try{
+
+                    if(response == null){
+                        UtilsLib.msgSelect(getApplicationContext(), "Error al traer las eventos.");
+                        return;
+                    }
+
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    RespuestaDTO resp = gson.fromJson(response.toString(),
+                            RespuestaDTO.class);
+
+                    if (resp == null || !resp.isCorrecto()) {
+                        if(TextUtils.isEmpty(resp.getMensaje())){
+                            UtilsLib.msgSelect(getApplicationContext(), "Error al traer las eventos.");
+                        } else {
+                            UtilsLib.msgSelect(getApplicationContext(), resp.getMensaje());
+                        }
+                        return;
+                    }
+
+                    JSONArray jsonArray = response.getJSONArray(Constantes.DATE_NODE_REST_API);
+                    ArrayList<EventoDTO> eventos = new Gson().fromJson(jsonArray.toString(),
+                            new TypeToken<List<EventoDTO>>() {
+                            }.getType());
+
+                    if(eventos == null || eventos.size() == 0){
+                        UtilsLib.msgSelect(getApplicationContext(), "No hay eventos en el calendario.");
+                        return;
+                    }
+
+                    Intent i = new Intent(getBaseContext(), Calendario2Activity.class);
+                    i.putParcelableArrayListExtra(Calendario2Activity.ARGS_EVENTS, eventos);
+                    startActivity(i);
+
+                } catch (Exception e){
+                    Log.d(LOG_TAG, "Error en el metodo marcaEventos(), causa: "+e.toString());
+                }
+            }
+        }).execute(Constantes.URL_GET_EVENTOS);
     }
 }
